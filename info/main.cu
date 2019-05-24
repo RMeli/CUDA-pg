9f51526a84e0e1c66fd5fbd45fb845278182115c
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-05-24 16:32:54 +0100

    device info

---
+++ info/main.cu
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int nGPUs;
+    cudaGetDeviceCount(&nGPUs);
+
+    for(int i{0}; i < nGPUs; i++){
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+    
+        cout << "Device #" << i << endl;
+        cout << "   Name: " << prop.name << endl;
+        cout << "   Global Memory (GN): " << prop.totalGlobalMem / 1e9 << endl;
+        cout << "   Clock Rate (MHz): " << prop.clockRate / 1e3 << endl;
+    }
+
+    return 0;
+}
+
