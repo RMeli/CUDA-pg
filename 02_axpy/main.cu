c1bea1aeb9b61392eadfdca74e7bf0304539e122
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2019-05-27 21:24:48 +0100

    better timing

--- 02_axpy/main.cu
+++ 02_axpy/main.cu
@@ -4,36 +4,40 @@
 
 #include "mem.h"
 #include "num.h"
+#include "timing.h"
 
 #include "axpy.cu"
 
 using namespace std;
 
-using duration = chrono::duration<double>;
-
 int main(){
     constexpr std::size_t n{1024};
 
+    constexpr std::size_t reps{10000};
+
     double x{1.3}, y{1.6}, a{2.4};
 
-    double* x_host = malloc_host(n, x);
-    double* y_host = malloc_host(n, y);
+    Timer t;
+    double time;
 
-    duration time;
+    double* x_host{nullptr};
+    double* y_host{nullptr};
 
     cout << "axpy (cpu)... ";
-    auto ti = chrono::high_resolution_clock::now();
-    axpy::axpy_cpu(y_host, x_host, a, n);
-    auto tf = chrono::high_resolution_clock::now();
-    time = chrono::duration_cast<duration>(tf - ti);
-    cout << time.count() << " seconds" << endl;
+    t.start();
+    for(std::size_t i{0}; i < reps; i++){
+        x_host = malloc_host(n, x);
+        y_host = malloc_host(n, y);
+        axpy::axpy_cpu(y_host, x_host, a, n);
+        free_host(x_host);
+        free_host(y_host);
+    }
+    time = t.stop();
+    cout << time << " ms" << endl;
 
     for(std::size_t i{0}; i < n; i++){
         assert(nearly_equal(y_host[i], y + a * x));
     }
-
-    free_host(x_host);
-    free_host(y_host);
 
     return 0;
 }
