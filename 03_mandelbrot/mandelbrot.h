3c714a08f66d5adb354de103fbdd1ad43dd4859c
Rocco Meli <r.meli@bluemail.ch>
2019-06-14 00:13:13 +0100

    formatting mandelbrot

--- 03_mandelbrot/mandelbrot.h
+++ 03_mandelbrot/mandelbrot.h
@@ -3,9 +3,11 @@
 
 #include <cstddef>
 
-void mandelbrot(char* image, std::size_t width, std::size_t height, std::size_t max_iters = 1000);
+void mandelbrot(char *image, std::size_t width, std::size_t height,
+                std::size_t max_iters = 1000);
 
-__global__
-void mandelbrot_gpu(char* image, std::size_t width, std::size_t height, std::size_t max_iters = 1000);
+__global__ void mandelbrot_gpu(char *image, std::size_t width,
+                               std::size_t height,
+                               std::size_t max_iters = 1000);
 
 #endif // MANDELBROT_H
