043ede345a94d302d8f267a7e5a0eca3a6a66093
Rocco Meli <rocco.meli@biodtp.ox.ac.uk>
2021-09-06 23:07:41 +0100

    add comments about ray tracing

--- 06_raytracer/sphere.h
+++ 06_raytracer/sphere.h
@@ -12,6 +12,11 @@ constexpr double lowest{std::numeric_limits<double>::lowest()};
 struct Sphere {
     // Check if ray shoot from pixel (ox, oy) hits the sphere
     __host__ __device__ double hit(double ox, double oy, double* n) {
+        // A ray is shoot from pixel at position (ox, oy)
+        // Ray travels only along z axis and the camera is at the origin
+
+        // Compute distance on the (x, y) plane from the pixel to the sphere
+        // center
         double dx = ox - x;
         double dy = oy - y;
 
@@ -19,15 +24,20 @@ struct Sphere {
         double dy2 = dy * dy;
         double radius2 = radius * radius;
 
-        // Check if ray from pixel (ox, oy) intercepts the sphere
+        // Check if ray from pixel (ox, oy) intercepts the sphere (cross
+        // section)
         if (dx2 + dy2 < radius2) {
             // Compute distance from the camera where the ray hits the sphere
+            // From the center of the sphere
             double dz{std::sqrt(radius2 - dx2 - dy2)};
             *n = dz / std::sqrt(radius2);
 
+            // Return actual distance travelled by the ray along z (before
+            // hitting)
             return dz + z;
         }
 
+        // Ray does not hit anything and travels to -infinity along z
         return lowest;
     }
 
